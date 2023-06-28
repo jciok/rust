47fda4b590a3f171b24f8f60977bfafa08b4a67b
jciok <jciok_wsl@mail.com>
2023-06-28 21:19:04 +0200

    ciekawe te koreańskie znaczki

--- first_step/src/main.rs
+++ first_step/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+
+    let number:u8 =100;
+    let slice = "Hello!";
+    let slice2 = "안녕!";
+
+    println!("The first number is {}, slice has {} bytes but also {} characters", number, slice.len(), slice.chars().count());
+    println!("Slice2 has {} bytes but only {} characters", slice2.len(), slice2.chars().count());
 }
